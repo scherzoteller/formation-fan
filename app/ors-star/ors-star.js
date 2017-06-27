a947d8050e031ea361097273b6120d52ee31ed7a
Scherzo <scherzoteller@bozo.com>
2017-06-27 16:24:52 +0200

    x

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -9,7 +9,17 @@
             controller: function OrsStarCtrl($element){
                 'ngInject';
                 console.log('inside controller', arguments);
-                let html = 'toto';
+                let html = '';
+                let note = 3;
+                let noteMax = 5;
+                for(let i=0; i<note; i++){
+                    console.log('yellow');
+                    html += '<img src="ors-star/img/yellow_star.png"/>';
+                }
+                for(let i=note; i<noteMax; i++){
+                    console.log('white');
+                    html += '<img src="ors-star/img/white_star.png"/>';
+                }
                 $element.html(html);
             }
 		};
