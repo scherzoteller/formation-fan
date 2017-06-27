707eda87d4c557d4d591745d68420a8511a7bd59
Scherzo <scherzoteller@bozo.com>
2017-06-27 16:58:22 +0200

    x

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -7,24 +7,37 @@
         return {
             restrict: 'E',
             scope: {
+                // double data binding
                 note: '=?',
                 noteMax: '=?'
+                // simple data binding (don't propagate to outside)
+                /*note: '<?',
+                noteMax: '<?'*/
             },
-            controller: function OrsStarCtrl($scope, $element) {
+            controller: function OrsStarCtrl($scope, $element, $compile) {
                 'ngInject';
                 console.log('inside controller', arguments);
-                let html = '';
-                let note = $scope.note || 0;
-                let noteMax = $scope.noteMax || 5;
-                for (let i = 0; i < note; i++) {
-                    console.log('yellow');
-                    html += '<img src="ors-star/img/yellow_star.png"/>';
-                }
-                for (let i = note; i < noteMax; i++) {
-                    console.log('white');
-                    html += '<img src="ors-star/img/white_star.png"/>';
-                }
-                $element.html(html);
+                $scope.updateNote = function updateNote(newNote) {
+                    console.log('new note: ', newNote);
+                    $scope.note = newNote;
+                };
+                $scope.$watch('note', function callB() {
+                    let html = '';
+                    let note = $scope.note || 0;
+                    let noteMax = $scope.noteMax || 5;
+                    if (note > 5) note = 5;
+                    if (note < 0) note = 0;
+                    for (let i = 0; i < note; i++) {
+                        html += '<img ng-click="updateNote(' + (i + 1) + ')" src="ors-star/img/yellow_star.png"/>';
+                    }
+                    for (let i = note; i < noteMax; i++) {
+                        // notation ES6
+                        html += `<img ng-click="updateNote(${i + 1})" src="ors-star/img/white_star.png"/>`;
+                    }
+                    $element.html(html);
+                    // recompile html code that was generated in that function (otherwise updateNote won't be recognized)
+                    $compile($element.contents())($scope);
+                });
             }
         };
     });
