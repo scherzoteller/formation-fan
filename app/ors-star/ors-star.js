60a5ca0d797b094093f57d9e733f40bb0c2416a5
Scherzo <scherzoteller@bozo.com>
2017-06-27 16:32:02 +0200

    x

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -1,28 +1,32 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	var app = angular.module('ors-star', []);
+    var app = angular.module('ors-star', []);
 
-	app.directive('orsStar', function () { 
-		return {
-			restrict: 'E',
-            controller: function OrsStarCtrl($element){
+    app.directive('orsStar', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                note: '=?',
+                noteMax: '=?'
+            },
+            controller: function OrsStarCtrl($scope, $element) {
                 'ngInject';
                 console.log('inside controller', arguments);
                 let html = '';
-                let note = 3;
-                let noteMax = 5;
-                for(let i=0; i<note; i++){
+                let note = $scope.note || 0;
+                let noteMax = $scope.noteMax || 5;
+                for (let i = 0; i < note; i++) {
                     console.log('yellow');
                     html += '<img src="ors-star/img/yellow_star.png"/>';
                 }
-                for(let i=note; i<noteMax; i++){
+                for (let i = note; i < noteMax; i++) {
                     console.log('white');
                     html += '<img src="ors-star/img/white_star.png"/>';
                 }
                 $element.html(html);
             }
-		};
-	});
+        };
+    });
 
 })();
