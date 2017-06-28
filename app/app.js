33313eb39ca2d0723c676058047ceb2d9bf7b140
Scherzo <scherzoteller@bozo.com>
2017-06-28 10:17:20 +0200

    commit all...

--- app/app.js
+++ app/app.js
@@ -1,26 +1,38 @@
-(function () {
-	'use strict';
-	var app = angular.module('main', ['ors-star']);
+// import './css/skel.css';
+// import './css/style.css';
+// import './css/style-xlarge.css';
 
-	app.directive('orsHeader', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-header.html'
-		};
-	});
+import 'angular/angular-csp.css';
+import './style.scss';
 
-	app.directive('orsBody', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-body.html'
-		};
-	});
+import 'angular';
+import './ors-star/ors-star.js';
 
-	app.directive('orsFooter', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-footer.html'
-		};
-	});
+var app = angular.module('main', ['ors-star']);
 
-})();
+import orsHeaderUrl from './tmpl/ors-header.html';
+const orsBodyUrl = require('./tmpl/ors-body.html');
+const orsFooterUrl = require('./tmpl/ors-footer.html');
+
+app.directive('orsHeader', function () {
+	return {
+		restrict: 'E',
+		template: orsHeaderUrl
+	};
+});
+
+app.directive('orsBody', function () {
+	return {
+		restrict: 'E',
+		template: orsBodyUrl
+	};
+});
+
+app.directive('orsFooter', function () {
+	return {
+		restrict: 'E',
+		template: orsFooterUrl
+	};
+});
+
+
--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -1,45 +1,39 @@
-(function () {
-    'use strict';
+const app = angular.module('ors-star', []);
 
-    var app = angular.module('ors-star', []);
+app.directive('orsStar', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			note: '=?'
+		},
+		controller: function OrsStarCtrl($scope, $element, $compile) {
+			'ngInject';
+			console.log('OrsStarCtrl', arguments);
+			$scope.update = function update(newNote) {
+				console.log('newNote', newNote);
+				$scope.note = newNote;
+			};
+			$scope.$watch('note', function () {
+				console.log('test à chaud');
+				let note = $scope.note || 3;
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
+				let html = '';
+				for (let i = 0; i < note; i++) {
+					html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png">';
+				}
+				for (let i = note; i < 5; i++) {
+					html += `
+<img  
+ng-click="update(${i + 1})" 
+src="./ors-star/img/white_star.png">`;
+				}
+				$element.html(html);
+				$compile($element.contents())($scope);
+			});
 
-    app.directive('orsStar', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                // double data binding
-                note: '=?',
-                noteMax: '=?'
-                // simple data binding (don't propagate to outside)
-                /*note: '<?',
-                noteMax: '<?'*/
-            },
-            controller: function OrsStarCtrl($scope, $element, $compile) {
-                'ngInject';
-                console.log('inside controller', arguments);
-                $scope.updateNote = function updateNote(newNote) {
-                    console.log('new note: ', newNote);
-                    $scope.note = newNote;
-                };
-                $scope.$watch('note', function callB() {
-                    let html = '';
-                    let note = $scope.note || 0;
-                    let noteMax = $scope.noteMax || 5;
-                    if (note > 5) note = 5;
-                    if (note < 0) note = 0;
-                    for (let i = 0; i < note; i++) {
-                        html += '<img ng-click="updateNote(' + (i + 1) + ')" src="ors-star/img/yellow_star.png"/>';
-                    }
-                    for (let i = note; i < noteMax; i++) {
-                        // notation ES6
-                        html += `<img ng-click="updateNote(${i + 1})" src="ors-star/img/white_star.png"/>`;
-                    }
-                    $element.html(html);
-                    // recompile html code that was generated in that function (otherwise updateNote won't be recognized)
-                    $compile($element.contents())($scope);
-                });
-            }
-        };
-    });
+		}
+	};
+});
 
-})();
+
